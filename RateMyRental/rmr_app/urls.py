d0a31c3c5392a10d9f2345d3fb6ea4b8194b9c93
Angelo <herman256561@gmail.com>
2023-11-06 18:21:27 -0500

    remove two folders

--- RateMyRental/rmr_app/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.rmr_app, name='rmr_home'),
-]
--- RateMyRental/rmr_app/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-def rmr_app(request):
-    return HttpResponse("Hello world!")
