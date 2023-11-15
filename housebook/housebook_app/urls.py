2e66b339b17e6c83e9d2e015ce2a53c6ad219373
Angelo <herman256561@gmail.com>
2023-11-14 19:35:06 -0500

    index page draft

--- housebook/housebook_app/urls.py
+++ housebook/housebook_app/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.housebook_app, name='housebook_home'),
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
